9569815b11c72258298fecc8586ed12a67c10dd4
Antelmo Verdugo <antelmogg@outlook.com>
2024-11-25 10:48:42 -0700

    auth: gestión de permisos

--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,45 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config');
+const secret = config.jwt.secret;
 
 function sign(data) {
-  return jwt.sign(data, 'secret');
+  return jwt.sign(data, secret);
+}
+
+function verify(token) {
+  return jwt.verify(token, secret)
+}
+
+function getToken(authorization) {
+  if (!authorization) {
+    throw new Error('No token');
+  }
+
+  if (authorization.indexOf('Bearer ') === -1) {
+    throw new Error('Invalid format');
+  }
+
+  let token = authorization.replace('Bearer ', '');
+  return token;
+}
+
+function decodeHeader(req) {
+  const authorization = req.headers.authorization || '';
+  const token = getToken(authorization);
+  const decoded = verify(token);
+
+  req.user = decoded;
+  return decoded;
+}
+
+const check = {
+  own: function (req, owner) {
+    const decodedToken = decodeHeader(req);
+    console.log(decodedToken);
+  }
 }
 
 module.exports = {
   sign,
+  check
 };
